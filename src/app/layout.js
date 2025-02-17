ff118ee6d3e3d8e26dc80450f062af2185285458
Kelvin Ingrit <kelviningrit753@gmail.com>
2025-02-17 11:28:38 -0500

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+// import { Inter } from "next/font/google";
+import localFont from 'next/font/local'
+import "./globals.css";
+
+const local = localFont({
+  src: '../../public/Gulya.otf',
+  weight: '400',
+  style: 'normal',
+})
+// const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "RocketShoot Online To The Moon",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={local.className}>{children}</body>
+    </html>
+  );
+}
